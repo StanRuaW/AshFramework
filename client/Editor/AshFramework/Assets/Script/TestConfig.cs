ba552a6e6227472748d0c6b18246badfc6450471
LAPTOP-461C8H42\45735 <457354109@qq.com>
2021-08-22 23:24:34 +0800

    【功能】【编辑器】可选的更好的选择下拉界面

--- client/Editor/AshFramework/Assets/Script/TestConfig.cs
+++ client/Editor/AshFramework/Assets/Script/TestConfig.cs
@@ -1,26 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TestConfig : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        var tables = new cfg.Tables(file =>
-    new Bright.Serialization.ByteBuf(
-        System.IO.File.ReadAllBytes("./Assets/Config/output_data" + "/" + file)));//TODO:Â·¾¶¹ÜÀí
-
-        tables.TbFullTypes.ForeachCfg((cfg.test.DemoType2 cfg) =>
-        {
-            Debug.Log(cfg.X6);
-            return true;
-        });
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [AssetReference] public Texture te;
 }
