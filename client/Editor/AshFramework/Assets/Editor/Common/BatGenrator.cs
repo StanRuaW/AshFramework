7c4cdf3a85bd7cc9bc659eae80d36ea835849b71
StanRua <457354109@qq.com>
2021-08-24 19:53:54 +0800

    【增加】【编辑器】cmd运行器v0.1

---
+++ client/Editor/AshFramework/Assets/Editor/Common/BatGenrator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EditorEx
+{
+    public class BatGenrator
+    {
+        private 
+
+
+        void GenerateBat() { }
+        protected void AddLine(string) { }
+        void RunBat() { }
+    }
+
+}
---
+++ client/Editor/AshFramework/Assets/Editor/Common/CmdCaller.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using CliWrap;
+
+namespace EditorEx
+{
+    public class CmdCaller
+    {
+        public static CMD CreateCaller()
+        {
+            return new CMD();
+        }
+
+        public class CMD
+        {
+            private string commands;
+            private string arguments;
+            private string workDictionary = "./";
+            public CMD AddCommand(string line)
+            {
+                //TODO:stringbuilder?
+                commands += "\n" + line;
+                return this;
+            }
+
+            public CMD AddArgument(string argA, string argB)
+            {
+                //TODO:stringbuilder?
+                arguments += "--" + argA + " " + argB;
+                return this;
+            }
+
+            public CMD SetDictionary(string dic)
+            {
+                //TODO:验证地址合法性
+                workDictionary = dic;
+                return this;
+            }
+
+            public void RunCmd()
+            {
+                //TODO:await
+                var result = Cli.Wrap(commands).WithArguments(arguments).WithWorkingDirectory(workDictionary).ExecuteAsync();
+                //TODO:log结果，缓存log文件
+            }
+        }
+    }
+}
