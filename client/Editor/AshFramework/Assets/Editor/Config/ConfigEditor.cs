37450ee6afc524dafcbe06401a7d9bcbd4794210
LAPTOP-461C8H42\45735 <457354109@qq.com>
2021-08-26 01:30:08 +0800

    【修改】【编辑器】配置表一键导出工具开个头

--- client/Editor/AshFramework/Assets/Editor/Config/ConfigEditor.cs
+++ client/Editor/AshFramework/Assets/Editor/Config/ConfigEditor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConfigServer : MonoBehaviour
+public class ConfigEditor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ client/Editor/AshFramework/Assets/Editor/Config/LubanGenMgr.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using EditorEx;
+using CliWrap.Buffered;
+
+namespace cfg
+{
+	public class LubanGenMgr
+	{
+		private static readonly string _clientName = "Luban.Client.exe";
+		private static readonly string _clientPath = @"D:\AshFrame\luban\src\Luban.Client\bin\Debug\net5.0";
+
+		public async Task<BufferedCommandResult> BuildAllConfigAsync()
+		{
+			//TODO：会不会有多次执行的冲突
+			_cmd = CMD.GetNewCaller( _clientName, _clientPath );
+
+			if (string.IsNullOrEmpty(_ip))
+			{
+				SetLocalIP();
+			}
+			_cmd.AddArgument( "-h", _ip );
+
+			//TODO:
+			_cmd.AddArgument( "-j", "cfg" );
+			_cmd.AddArgument( "--", "" );
+
+
+			throw new System.NotImplementedException();
+		}
+
+		public async Task<BufferedCommandResult> CleanAllConfigAsync()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void SetLocalIP()
+		{
+			_ip = "127.0.0.1";
+		}
+
+		private CMD _cmd;
+		private string _ip;
+
+		private void SetIP(string ip)
+		{
+			//TODO:check
+			_ip = ip;
+		}
+
+
+	}
+
+}
