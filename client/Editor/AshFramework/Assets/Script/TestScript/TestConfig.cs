621346a3bbf6a84c2e393bf5254572f71bfa4059
LAPTOP-461C8H42\45735 <457354109@qq.com>
2021-08-29 21:07:35 +0800

    【增加】【插件】Console log pro，功能挺多的，挺好

---
+++ client/Editor/AshFramework/Assets/Script/TestScript/TestConfig.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using EditorEx.SelectAndDropExtend;
+
+namespace TestEx
+{
+	public class TestConfig : MonoBehaviour
+	{
+
+		private void Start()
+		{
+			cfg.ConfigMgr.Inst.Initialize();
+
+			Debug.Log( cfg.ai.BehaviorTree.ID );
+			cfg.ConfigMgr.Inst.TbItem.ForeachCfg( ( item ) =>
+			{
+				Debug.Log( item.Desc );
+				return true;
+			} );
+
+			Debug.Log("ass");
+		}
+	}
+
+}
---
+++ client/Editor/AshFramework/Assets/Script/TestScript/TestLog.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("ass");
+		Debug.Log("#rua#ass");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
